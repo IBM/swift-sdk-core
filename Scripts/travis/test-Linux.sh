477562b39cfb03a2c16c98536c6fd5e2f8e72db5
repjarms <jeffarn12@gmail.com>
2019-09-24 13:50:11 -0400

    build(docker): update docker tests
    this includes the official image of swift 5.1

--- Scripts/travis/test-Linux.sh
+++ Scripts/travis/test-Linux.sh
@@ -14,7 +14,7 @@ fi
 GIT_PATH="https://github.com/$GIT_SLUG.git"
 
 # we want to use the dockerfile
-docker build - < ./Docker/$SWIFT_VERSION/Dockerfile -t IBMSwiftSDKCore:linux-tests-$SWIFT_VERSION --build-arg git_path=$GIT_PATH --build-arg git_branch=$TARGET_BRANCH --build-arg vcap_services=$VCAP_SERVICES --build-arg ibm_credentials_file=$IBM_CREDENTIALS_FILE
+docker build - < ./Docker/$SWIFT_VERSION/Dockerfile -t ibmswiftsdkcore:linux-tests-$SWIFT_VERSION --build-arg git_path=$GIT_PATH --build-arg git_branch=$TARGET_BRANCH --build-arg vcap_services=$VCAP_SERVICES --build-arg ibm_credentials_file=$IBM_CREDENTIALS_FILE
 
-docker run IBMSwiftSDKCore:linux-tests-$SWIFT_VERSION
+docker run ibmswiftsdkcore:linux-tests-$SWIFT_VERSION
 docker system prune -a -f
